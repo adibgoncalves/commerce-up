cf6fc8d5bfdaa682f16d977f805765891cd8f913
Adib Gonçalves <adib.fernandes@gmail.com>
2020-09-17 21:13:18 -0300

    Ajustes na responsividade e finalização do menu responsivo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:0,
-    nav:true,
-    center: true,
-    items:1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsiveClass:true,
-    dots: true,
-    nav:false
-})
+var BTN = document.getElementById("nav-button")
+
+BTN.addEventListener("click", document.getElementById("nav-menu").classList.toggle("active"));
